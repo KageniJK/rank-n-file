b21cb3502c2383f4498ec4f8eef296ad0c552ce9
James Kageni <james.kageni@gmail.com>
2018-10-15 16:41:28 +0300

    added the login modal

--- file/models.py
+++ file/models.py
@@ -1,5 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    """
+    Extends the basic Django user model
+    """
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    profile_pic = models.ImageField(upload_to='profiles/')
+    bio = models.CharField(max_length=250)
 
 
+class Project(models.Model):
+    """
+    class that defines the projects
+    """
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    name = models.CharField(max_length=50)
+    link = models.URLField()
+    description = models.CharField(max_length=250)
+    screenshot = models.ImageField(upload_to='screengrabs/')
+
+
+class Rating(models.Model):
+    """
+    class that defines the votes
+    """
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
--- rank/settings.py
+++ rank/settings.py
@@ -26,10 +26,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = config('SECRET_KEY')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+ALLOWED_HOSTS = []
 
 
 # Application definition
